d6db49bd0601ecb50547039b15f33b16caaca26f
Mbring jeremie <mbringjeremys@gmail.com>
2022-03-17 03:30:53 +0100

    mise a jour de script envoie des mails

--- watch.sh
+++ watch.sh
@@ -12,21 +12,28 @@ file_created() {
 	echo "[$TIMESTAMP]: the file $1$2 was created $1">>monitor_log
 }
 
+do_some_action() {
+	file_created "./$1" "$2"
+	zip -r report.zip "./$1""$2"
+	gpg --batch --output  report.zip.gpg --passphrase mypassword --symmetric report.zip
+	mpack -s subject report.zip.gpg mbringjeremys@gmail.com
+	rm report.zip
+	rm report.zip.gpg
+}
+
 inotifywait -q -m -r -e modify,delete,create $1 | while read DIRECTORY EVENT FILE;do
 	case $EVENT in
 		MODIFY*)
 			file_modified "./$DIRECTORY" "$FILE"
+			do_some_action $DIRECTORY $FILE
 			;;
 		CREATE*)
 			file_created "./$DIRECTORY" "$FILE"
-			zip -r report.zip "./$DIRECTORY""$FILE"
-			gpg --batch --output  report.zip.gpg --passphrase mypassword --symmetric report.zip
-			mpack -s subject report.zip.gpg mbringjeremys@gmail.com
-			rm report.zip
-			rm report.zip.gpg
+			do_some_action $DIRECTORY $FILE
 			;;
 		DELETE*)
 			file_removed "./$DIRECTORY" "$FILE"
+			do_some_action $DIRECTORY $FILE
 			;;
 	esac
 done
